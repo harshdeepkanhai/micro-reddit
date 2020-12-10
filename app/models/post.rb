6254a177c57c36823daadad50a7e9f6cb7996385
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-12-11 01:27:00 +0530

    Add Post model validations and associations with User model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { in: 20..80 }, uniqueness: true
+  validates :body, presence: true, length: { in: 200..10000}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_192607) do
+ActiveRecord::Schema.define(version: 2020_12_10_194423) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "name"
@@ -18,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_12_10_192607) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
